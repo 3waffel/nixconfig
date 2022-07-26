d1a8ae136d9b7a38660f4e6241f5d160b0ef6ad6
ワフ <45911671+3waffel@users.noreply.github.com>
2022-07-26 10:50:24 +0000

    edit nixos/modules/nix.nix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -13,19 +13,21 @@
 
     gc = {
       automatic = true;
-      dates = "monthly";
-      options = "--delete-older-than 30d";
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
 
     settings = {
       auto-optimise-store = true;
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
         "https://3waffel.cachix.org"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "3waffel.cachix.org-1:Tm5oJGJA8klOLa4dYRJvoYWQIpItX+0w9KvoRP8Z2mc="
       ];
     };
