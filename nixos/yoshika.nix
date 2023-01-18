99c6e1a1ef78d5ed6d574ff0fadb677424350f6e
ワフ <45911671+3waffel@users.noreply.github.com>
2023-01-18 14:30:19 +0000

    add wsl installer ci workflow and fix systemd error

--- nixos/yoshika.nix
+++ nixos/yoshika.nix
@@ -30,20 +30,35 @@
     enable = true;
     defaultUser = "wafu";
     startMenuLaunchers = true;
+    nativeSystemd = true;
     wslConf.automount.root = "/mnt";
     wslConf.network.generateResolvConf = false;
     docker-native.enable = true;
   };
 
-  environment.etc."resolv.conf" = {
-    text = ''
-      nameserver 1.1.1.1
-      nameserver 8.8.8.8
-      nameserver 101.6.6.6
-    '';
+  # fix systemd error
+  systemd.services.nixs-wsl-systemd-fix = {
+    description = "Fix the /dev/shm symlink to be a mount";
+    unitConfig = {
+      DefaultDependencies = "no";
+      Before = ["sysinit.target" "systemd-tmpfiles-setup-dev.service" "systemd-tmpfiles-setup.service" "systemd-sysctl.service"];
+      ConditionPathExists = "/dev/shm";
+      ConditionPathIsSymbolicLink = "/dev/shm";
+      ConditionPathIsMountPoint = "/run/shm";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "${pkgs.coreutils-full}/bin/rm /dev/shm"
+        "/run/wrappers/bin/mount --bind -o X-mount.mkdir /run/shm /dev/shm"
+      ];
+    };
+    wantedBy = ["sysinit.target"];
   };
+
   environment.variables.EDITOR = "nano";
 
   system.stateVersion = "22.05";
   networking.hostName = "yoshika";
+  networking.nameservers = ["1.1.1.1" "8.8.8.8" "101.6.6.6"];
 }
